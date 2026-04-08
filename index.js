68ab24aee56361eca58b8acd0c80dbd16a10f4fa
nexacorpesport-pixel <nexa.corp.esport@gmail.com>
2026-04-09 00:13:57 +0200

    Update developer status from 'zxlorz' to 'Logs'

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.once('clientReady', async () => {
         const statuses = [
             { name: `${memberCount} membres`, type: ActivityType.Streaming, url: "https://twitch.tv/kyrelfn" },
             { name: "Surveille les membres", type: ActivityType.Streaming, url: "https://twitch.tv/kyrelfn" },
-            { name: "Dev by zxlorz", type: ActivityType.Streaming, url: "https://twitch.tv/kyrelfn" }
+            { name: "Dev by Logs", type: ActivityType.Streaming, url: "https://twitch.tv/kyrelfn" }
         ];
 
         const randomStatus = statuses[Math.floor(Math.random() * statuses.length)];
