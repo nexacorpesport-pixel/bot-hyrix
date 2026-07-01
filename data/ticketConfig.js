9995ca54cae0121914ab91aa85c8bc356ad67764
nexacorpesport-pixel <nexa.corp.esport@gmail.com>
2026-07-01 19:06:57 +0200

    Update ticketConfig.js

--- data/ticketConfig.js
+++ data/ticketConfig.js
@@ -1,15 +1,15 @@
 module.exports = {
 
-    PANEL_CHANNEL: "1505330772343656680",
-    TEST_MODO_ROLE: "1505330702793576568", // Ton rôle Test Modérateur
+    PANEL_CHANNEL: "1501626016815382669",
+    TEST_MODO_ROLE: "1501625955427422378", // Ton rôle Test Modérateur
 
     CATEGORIES: {
-        staff: "1505559323105951744",
-        joueur: "1505559260975595753",
-        audiovisuel: "1506771653621710848",
-        aide: "1505559399442284605",
-        partenariat: "1506771703769071726",
-        autre: "1506771703769071726"
+        staff: "1521924264238780559",
+        joueur: "1521924347227410565",
+        audiovisuel: "1521924438113779922",
+        aide: "1521924495101923569",
+        partenariat: "1521924558737768643",
+        autre: "1521924607106482196"
     },
 
     ROLES: {
@@ -30,13 +30,13 @@ module.exports = {
     },
 
     PR_ROLES: [
-        { min: 0, max: 20, role: "1505330730262200382", name: "GRINDER 5" },
-        { min: 20, max: 40, role: "1505330728655913113", name: "GRINDER 4" },
-        { min: 40, max: 60, role: "1505330726185341079", name: "GRINDER 3" },
-        { min: 60, max: 80, role: "1505330724063019132", name: "GRINDER 2" },
-        { min: 80, max: 100, role: "1505330723010248806", name: "GRINDER 1" },
-        { min: 100, max: 350, role: "1505330720380420219", name: "ESPOIR" },
-        { min: 350, max: 1000, role: "1505330715938525204", name: "ACADEMIE" },
-        { min: 1000, max: 999999, role: "1505330713891963061", name: "JOUEUR OFFICIEL" }
+        { min: 0, max: 20, role: "1501920524547133560", name: "GRINDER 5" },
+        { min: 20, max: 40, role: "1501920490107965620", name: "GRINDER 4" },
+        { min: 40, max: 60, role: "1501920467475239013", name: "GRINDER 3" },
+        { min: 60, max: 80, role: "1501920444054376448", name: "GRINDER 2" },
+        { min: 80, max: 100, role: "1501920404686635008", name: "GRINDER 1" },
+        { min: 100, max: 350, role: "1501920362491936818", name: "ESPOIR" },
+        { min: 350, max: 1000, role: "1501625971420434515", name: "ACADEMIE" },
+        { min: 1000, max: 999999, role: "1501625970564534304", name: "JOUEUR OFFICIEL" }
     ]
 };
